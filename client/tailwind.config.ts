5613e1d345bafe413c7b5afc3b651f4e80077f62
akanksha9975 <akankshayadav9975@gmail.com>
2025-08-28 10:00:32 +0530

    new

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{jsx,tsx}", "./*.html"],
+    content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx,css}"],
     theme: {
         extend: {
             colors: {
